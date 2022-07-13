11557d33a83d55329418a1787ca4a4586131b822
Leonid Khitskov <ivanovichivanivanov@protonmail.ch>
2022-07-14 05:27:34 +0900

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let newStr = str.split('');
+  for (let l = 0; l < str.length / 2; l++) {
+    newStr.forEach((el, i) => {
+      bracketsConfig.forEach(br => {
+        if (br[0] === el && newStr[i + 1] === br[1]) {
+          newStr[i] = ' ';
+          newStr[i + 1] = ' ';
+        }
+      })
+    });
+    newStr = newStr.filter(el => el !== ' ');
+  }
+  return newStr.length > 0 ? false : true;
 }
